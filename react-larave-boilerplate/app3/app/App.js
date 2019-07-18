3fc58d6baa2165a8e07c1a2362d958d98f7f335c
Muhammad Taha <taha_khan198923@yahoo.com>
2019-07-18 23:26:14 +0500

    work on native mobile app

---
+++ react-larave-boilerplate/app3/app/App.js
@@ -0,0 +1,5 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import {createRootNavigator} from './router'
+
+export default createRootNavigator();
