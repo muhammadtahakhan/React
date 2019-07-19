d770af4c2df9450019893fd4e6741b687cd7cd50
Muhammad Taha <taha_khan198923@yahoo.com>
2019-07-19 12:26:30 +0500

    tabs with icons working now

--- react-larave-boilerplate/app3/App.js
+++ react-larave-boilerplate/app3/App.js
@@ -29,50 +29,10 @@ import {
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import { Dimensions, Platform } from 'react-native';
 import { StackNavigator,  createBottomTabNavigator } from 'react-navigation';
-import Bookcase from './app/screens/Bookcase';
-import Explore from './app/screens/Explore';
-import AddBook from './app/screens/AddBook';
-import Lists from './app/screens/Lists';
-import Profile from './app/screens/Profile';
+import { Icon } from 'react-native-elements';
 
+import { TabNavigator } from './app/router';
 
-const TabNavigator = createBottomTabNavigator({
-  'Bookcase': {
-    screen: Bookcase,
-    navigationOptions: {
-      tabBarLabel: 'Bookcase',
-      // tabBarIcon: ({ tintColor }) => <Icon name="open-book" type="entypo" size={28} color={tintColor} />
-    },
-  },
-  'Explore': {
-    screen: Explore,
-    navigationOptions: {
-      tabBarLabel: 'Explore',
-      // tabBarIcon: ({ tintColor }) => <Icon name="ios-map-outline" type="ionicon" size={28} color={tintColor} />
-    },
-  },
-  'Add Book': {
-    screen: AddBook,
-    navigationOptions: {
-      tabBarLabel: 'Add Book',
-      // tabBarIcon: ({ tintColor }) => <Icon name="ios-add-circle-outline" type="ionicon" size={28} color={tintColor} />
-    },
-  },
-  'Lists': {
-    screen: Lists,
-    navigationOptions: {
-      tabBarLabel: 'Lists',
-      // tabBarIcon: ({ tintColor }) => <Icon name="list" type="entypo" size={28} color={tintColor} />
-    },
-  },
-  'My Profile': {
-    screen: Profile,
-    navigationOptions: {
-      tabBarLabel: 'Profile',
-      // tabBarIcon: ({ tintColor }) => <Icon name="ios-person-outline" type="ionicon" size={28} color={tintColor} />
-    },
-  },
-});
 
 const AppNavigator = createStackNavigator({
   Home: TabNavigator,
